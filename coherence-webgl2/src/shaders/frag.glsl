32476f838241589ab105ed971fd3bedb78a718db
Thomas Bénéteau <thomas@bitwise.me>
2019-09-27 03:11:36 +1200

    Add visibility testing function (not used yet)

--- coherence-webgl2/src/shaders/frag.glsl
+++ coherence-webgl2/src/shaders/frag.glsl
@@ -66,7 +66,6 @@ bool eval_sdf(ray_t ray, uint geometry, uint instance, inout vec2 range) {
 // this only holds true if the ray origin is actually inside the starting offset's AABB...
 // (to within the specified precision, i.e. it must be othervise visited during traversal)
 // (if not this MAY LOOP FOREVER! must be absolutely surely, provably inside the AABB)
-// if this property is not needed we can do some more micro-optimizations
 
 traversal_t traverse_scene(ray_t ray) {
     traversal_t traversal = traversal_prepare(PREC * 100.0);
@@ -90,11 +89,42 @@ traversal_t traverse_scene(ray_t ray) {
                 traversal_record_hit(traversal, range.x, uvec2(word1, word2));
             }
         } else if (word2 == 0xffffffffU) {
-            index = word1; // skip branch
+            index = word1;
         }
     } while (index != 0U);
 
     return traversal;
+}
+
+// Tests whether a ray intersects any geometry in the scene. This is much faster
+// than finding the closest intersection and is intended for visibility testing.
+
+bool is_ray_occluded(ray_t ray, float distance) {
+    vec3 idir = vec3(1.0) / ray.dir;
+
+    uint index = 0U;
+
+    do {
+        BvhNode node = instance_buffer.data[index++];
+
+        uint word1 = floatBitsToUint(node.data1.w);
+        uint word2 = floatBitsToUint(node.data2.w);
+
+        index *= uint((word1 & 0x00008000U) == 0U);
+        word1 &= 0xffff7fffU; // remove cyclic bit
+
+        vec2 range = vec2(PREC * 100.0, distance);
+
+        if (ray_bbox(ray.org, idir, range, node.data1.xyz, node.data2.xyz)) {
+            if (word2 != 0xffffffffU && eval_sdf(ray, word1 & 0xffffU, word1 >> 16U, range)) {
+                return true;
+            }
+        } else if (word2 == 0xffffffffU) {
+            index = word1;
+        }
+    } while (index != 0U);
+
+    return false;
 }
 
 // Low-discrepancy sequence generator.
@@ -388,7 +418,7 @@ void main() {
             // generally flat, zero-volume SDFs where this issue would occur
             // I think it cannot occur if the SDF has any volume at all
 
-            ray.org += ray.dir * (traversal.range.y - PREC); // closest distance to hit
+            ray.org += ray.dir * (traversal.range.y - PREC);
 
             vec3 normal = sdf_normal(traversal.hit.x & 0xffffU, traversal.hit.x >> 16U, ray.org);
 
