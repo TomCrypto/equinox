d4e260c4e2d282e591f54f8f93796a94099e6d8a
Thomas Bénéteau <thomas@bitwise.me>
2019-10-13 21:37:23 +1300

    Cleanups

--- src/shaders/include/geometry.glsl
+++ src/shaders/include/geometry.glsl
@@ -9,8 +9,6 @@ vec3 geometry_normal(uint geometry, uint inst, vec3 p);
 
 bool ray_sdf(ray_t ray, uint geometry, uint instance, inout vec2 range) {
     while (range.x <= range.y) {
-        // need to take the absolute value here in case we're on the inside of a distance field
-        // I'm not sure if this is always valid?
         float dist = abs(geometry_distance(geometry, instance, ray.org + range.x * ray.dir));
 
         if (dist < PREC * 0.1) {
