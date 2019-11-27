2cb46068b43ea58b74437d05bf073d133ea7aa9b
Thomas Bénéteau <thomas@bitwise.me>
2019-11-27 20:45:22 +1300

    WIP fixes to delete button

--- viewer/src/components/SaveLoadEditor.vue
+++ viewer/src/components/SaveLoadEditor.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="scene-list">
-    <button class="button save-scene" v-on:click="saveScene()" title="Save the current scene">
-      <div class="save-text">
-        <font-awesome-icon icon="hdd" size="2x" />
-      </div>
-    </button>
+    <div class="scene-card">
+      <button class="button" v-on:click="saveScene()" title="Save the current scene">
+        <div class="save-icon">
+          <font-awesome-icon icon="hdd" size="2x" />
+        </div>
+      </button>
+    </div>
 
-    <button
-      class="button load-scene"
-      v-for="scene in displayScenes"
-      :key="scene.name"
-      :style="`background-image: url(${scene.thumbnail})`"
-      v-on:click="loadScene(scene.name)"
-    >
-      <div class="delete-scene" v-on:click="deleteScene($event, scene.name)">
-        <font-awesome-icon icon="trash" fixed-width />
+    <div class="scene-card" v-for="scene in displayScenes" :key="scene.name">
+      <button
+        :style="`background-image: url(${scene.thumbnail})`"
+        class="button scene-name-container"
+        v-on:click="loadScene(scene.name)"
+      >
+        <div class="scene-name">{{ scene.name }}</div>
+      </button>
+      <div class="delete-scene" v-on:click="deleteScene(scene.name)">
+        <font-awesome-icon class="delete-icon" icon="trash" size="lg" />
       </div>
-      <div class="scene-name">{{ scene.name }}</div>
-    </button>
+    </div>
   </div>
 </template>
 
@@ -84,13 +86,9 @@ export default class extends Vue {
     this.$root.$emit("save-scene-request", "Unnamed scene");
   }
 
-  private async deleteScene(event: Event, name: string) {
-    console.log(event);
-    event.preventDefault();
-    event.stopImmediatePropagation();
-
-    /*await this.store.removeItem(name);
-    this.updateFromStore();*/
+  private async deleteScene(name: string) {
+    await this.store.removeItem(name);
+    this.updateFromStore();
   }
 
   private async loadScene(name: string) {
@@ -138,35 +136,28 @@ export default class extends Vue {
   user-select: none;
 }
 
-.button {
+.scene-card {
+  display: flex;
   flex: 1;
   min-width: 320px;
   min-height: 180px;
-  text-decoration: none;
   margin: 3px;
-  background-color: black;
+  position: relative;
+}
+
+.button {
+  flex: 1;
+  width: 100%;
+  text-decoration: none;
   border-radius: 6px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-color: black;
   box-shadow: inset 0px 0px 5px 0px #555555;
   border: 1px solid #555555;
   cursor: pointer;
   padding-top: 4px;
-}
-
-.save-scene {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.load-scene {
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: contain;
-  align-items: stretch;
-  display: flex;
-  flex-direction: column-reverse;
-  position: relative;
 }
 
 .button:active {
@@ -177,6 +168,13 @@ export default class extends Vue {
 .button:focus {
   box-shadow: 0 0 1pt 1pt #7193d9;
   outline: 0;
+}
+
+.scene-name-container {
+  align-items: stretch;
+  display: flex;
+  flex-direction: column-reverse;
+  position: relative;
 }
 
 .scene-name {
@@ -192,25 +190,37 @@ export default class extends Vue {
   margin-right: 4px;
 }
 
-.save-text {
+.save-icon {
   color: #ffffff;
   font-size: 2em;
   flex: 0;
 }
 
 .delete-scene {
+  cursor: pointer;
   display: inline-block;
   position: absolute;
   margin: 8px;
   top: 0;
   right: 0;
-  width: 32px;
-  height: 32px;
+  width: 20px;
+  height: 22px;
   background-color: transparent;
   color: #ffffff;
   border: 0;
   outline: 0;
   text-align: center;
   vertical-align: middle;
+  z-index: 1;
+}
+
+.delete-icon:hover {
+  color: #aaaaff;
+  margin-top: 1px;
+}
+
+.delete-icon:active {
+  color: #aaaaff;
+  margin-top: 2px;
 }
 </style>
