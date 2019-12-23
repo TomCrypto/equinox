bac450c9ce918f46af3ad093c2bf98b8065a71da
Thomas Bénéteau <thomas@bitwise.me>
2019-12-24 00:08:46 +1300

    Restyle licensing tab a bit and add attribution for envmaps and textures
    Textures are still WIP in another branch

--- viewer/src/components/LicensingEditor.vue
+++ viewer/src/components/LicensingEditor.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="container">
-    <pre class="licensing">{{ licensing }}</pre>
+    <div class="attribution">
+      <p>
+        All environment maps from
+        <a href="https://hdrihaven.com/">HDRI Haven</a> and most textures from
+        <a href="https://texturehaven.com/">Texture Haven</a>.
+      </p>
+
+      <hr />
+    </div>
+
+    <div class="licensing">
+      <pre>{{ licensing }}</pre>
+    </div>
   </div>
 </template>
 
@@ -15,11 +27,33 @@ export default class extends Vue {
 
 <style scoped>
 .container {
+  overflow: hidden;
   display: flex;
   flex-direction: column;
+  height: 100%;
 }
 
 .licensing {
+  overflow: scroll;
   flex: 1;
+  position: relative;
+}
+
+.licensing pre {
+  height: 100%;
+}
+
+.attribution {
+  flex: 0;
+  text-align: center;
+}
+
+.attribution a {
+  color: #dd5522;
+}
+
+.attribution a:hover {
+  color: orange;
+  text-decoration: underline;
 }
 </style>
