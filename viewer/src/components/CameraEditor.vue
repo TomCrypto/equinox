1b1ce61eb5929915d2444c771a933738e6bf650d
Thomas Bénéteau <thomas@bitwise.me>
2020-08-08 01:22:01 +1000

    Add a quick UI for the lens flare

--- viewer/src/components/CameraEditor.vue
+++ viewer/src/components/CameraEditor.vue
@@ -116,6 +116,47 @@
           @dragging="changeApertureSides"
         />
       </div>
+      <div class="settings-cell settings-label">Lens Flare</div>
+      <div class="settings-cell">
+        <select
+          :value="lensFlareEnabled ? 'enabled' : 'disabled'"
+          @change="changeLensFlareEnabled($event.target.value)"
+          selected
+        >
+          <option value="disabled">Disabled</option>
+          <option value="enabled">Enabled</option>
+        </select>
+      </div>
+      <div class="settings-cell settings-label">Aperture Filter</div>
+      <div class="settings-cell">
+        <select
+          :value="apertureFilter"
+          :disabled="!lensFlareEnabled"
+          @change="changeApertureFilter($event.target.value)"
+          selected
+        >
+          <option
+            v-for="map in APERTURE_FILTERS"
+            :key="map.data"
+            :value="map.data"
+            >{{ map.name }}</option
+          >
+        </select>
+      </div>
+      <div class="settings-cell settings-label">Aperture Filter Tile Size</div>
+      <div class="settings-cell">
+        <select
+          :value="apertureFilterTileSize"
+          :disabled="!lensFlareEnabled"
+          @change="changeApertureFilterTileSize($event.target.value)"
+          selected
+        >
+          <option value="lowest">Lowest</option>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+      </div>
     </div>
   </div>
 </template>
@@ -144,12 +185,41 @@ interface SceneCamera {
   field_of_view: number;
 }
 
+interface SceneDisplay {
+  lens_flare_enabled: boolean;
+}
+
+interface SceneAperture {
+  filter: string;
+  tile_size: "lowest" | "low" | "medium" | "high";
+}
+
 @Component
 export default class extends Vue {
   @Prop() private scene!: WebScene;
 
   private changeApertureType(value: "point" | "circle" | "ngon") {
     this.apertureType = value;
+    this.update();
+  }
+
+  private changeLensFlareEnabled(value: "disabled" | "enabled") {
+    this.lensFlareEnabled = value === "enabled";
+
+    if (this.lensFlareEnabled && this.apertureFilter === null) {
+      this.apertureFilter = this.APERTURE_FILTERS[0].data;
+    }
+
+    this.update();
+  }
+
+  private changeApertureFilter(value: string) {
+    this.apertureFilter = value;
+    this.update();
+  }
+
+  private changeApertureFilterTileSize(value: "lowest" | "low" | "medium" | "high") {
+    this.apertureFilterTileSize = value;
     this.update();
   }
 
@@ -200,9 +270,12 @@ export default class extends Vue {
   focalDistance: number = 0;
   focalCurvature: number = 0;
   fieldOfView: number = 0;
+  lensFlareEnabled: boolean = false;
+  apertureFilter: string | null = null;
+  apertureFilterTileSize: "lowest" | "low" | "medium" | "high" = "low";
 
   created() {
-    const [_, camera] = this.getSceneData();
+    const [_, camera, aperture, display] = this.getSceneData();
 
     this.apertureType = camera.aperture.type;
 
@@ -220,10 +293,14 @@ export default class extends Vue {
     this.focalDistance = camera.focal_distance;
     this.focalCurvature = camera.focal_curvature;
     this.fieldOfView = camera.field_of_view;
+
+    this.lensFlareEnabled = display.lens_flare_enabled;
+    this.apertureFilter = aperture ? aperture.filter : null;
+    this.apertureFilterTileSize = aperture ? aperture.tile_size : "low";
   }
 
   private update() {
-    const [json, camera] = this.getSceneData();
+    const [json, camera, aperture, display] = this.getSceneData();
 
     camera.aperture.type = this.apertureType;
 
@@ -242,14 +319,40 @@ export default class extends Vue {
     camera.focal_curvature = this.focalCurvature;
     camera.field_of_view = this.fieldOfView;
 
+    display.lens_flare_enabled = this.lensFlareEnabled;
+
+    if (this.apertureFilter === null) {
+      json.aperture = null;
+    } else {
+      json.aperture = {
+        filter: this.apertureFilter,
+        tile_size: this.apertureFilterTileSize,
+      }
+    }
+
     this.scene.set_json(json);
   }
 
-  private getSceneData(): [any, SceneCamera] {
+  private getSceneData(): [any, SceneCamera, SceneAperture | null, SceneDisplay] {
     const json = this.scene.json();
 
-    return [json, json.camera];
+    return [json, json.camera, json.aperture, json.display];
   }
+
+  APERTURE_FILTERS = [
+    {
+      name: "Aperture Filter 1",
+      data: "aperture_filter_1.raw"
+    },
+    {
+      name: "Aperture Filter 2",
+      data: "aperture_filter_2.raw"
+    },
+    {
+      name: "Aperture Filter 3",
+      data: "aperture_filter_3.raw"
+    },
+  ]
 }
 </script>
 
